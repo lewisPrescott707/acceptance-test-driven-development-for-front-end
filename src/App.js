3a076326b93e321e27962f8cab1a031a668baabf
prescottlewis707 <prescottlewis707@gmail.com>
2022-10-07 09:37:37 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  const handleAdd = (newItem) => {
+    setItems(oldItems => oldItems.concat([newItem]));
+  };
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
