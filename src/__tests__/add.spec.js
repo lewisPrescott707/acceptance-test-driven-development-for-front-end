ac1d7f800853e90e2e9f5a08073a6649243629bc
prescottlewis707 <prescottlewis707@gmail.com>
2022-10-07 11:37:58 +0100

    chapter-5

---
+++ src/__tests__/add.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import Add from '../Add';
+
+describe('todo add', () => {
+    test('storing input value', async () => {
+        render(<Add />);
+
+        await userEvent.type(screen.getByTestId("todo-input"), "second todo item");
+
+        expect(screen.getByDisplayValue(/second todo item/)).toBeInTheDocument();
+    })
+})
