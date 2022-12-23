e0a97322f6f9d29b6c30bcd8e85fcb88da75d129
prescottlewis707 <prescottlewis707@gmail.com>
2022-12-23 14:20:46 +0000

    refactor onChange

--- src/Add.js
+++ src/Add.js
@@ -3,13 +3,14 @@ import { useState } from 'react';
 export default function Add() {
     const [data, setData] = useState({
         add_todo: ''
-    })
-    const onChange = (evt) => {
-        const key = evt.target.name;
-        const value = evt.target.value;
-        setData(oldData => ({ ...oldData, [key]: value }))
-    }
+    });
+
     return (
-        <input data-testid="todo-input" value={data.add_todo} onChange={onChange} name="add_todo" />
+        <input 
+            data-testid="todo-input" 
+            value={data.add_todo} 
+            onChange={(evt) => setData(oldData => ({ ...oldData, [evt.target.name]: evt.target.value }))}
+            name="add_todo" 
+        />
     );
 }
